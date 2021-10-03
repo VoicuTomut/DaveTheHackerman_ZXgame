26991780c9a5d010f41993fbf94b115557718e87
VoicuTomut <andrei.apetean@gmail.com>
2021-10-03 06:59:51 +0300

    tutorial

--- Assets/Scripts/UI/TutorialSelectionManager.cs
+++ Assets/Scripts/UI/TutorialSelectionManager.cs
@@ -8,9 +8,15 @@ using UnityEngine.UI;
 public class TutorialSelectionManager : MonoBehaviour
 {
     public List<TutorialEntry> gameMechanicsEntries;
+    public List<TutorialEntry> quantumMechanicsEntries;
+    public GameObject gmScrollArea;
+    public GameObject qmScrollArea;
+    public GameObject entryArea;
+    public GameObject storyArea;
     public CustomButton GM;
     public CustomButton QM;
     public CustomButton S;
+    public Sprite defaultImage;
 
     [SerializeField]
     private Image displayImage;
@@ -19,7 +25,9 @@ public class TutorialSelectionManager : MonoBehaviour
     [SerializeField]
     private GameObject button;
     [SerializeField]
-    private GameObject buttonPanel;
+    private GameObject gmContent;
+    [SerializeField]
+    private GameObject qmContent;
 
     private EntrySelection selection;
 
@@ -27,45 +35,63 @@ public class TutorialSelectionManager : MonoBehaviour
     private void OnEnable()
     {
         Init();
-        //firstSelection.Select();
+
     }
 
     public void Init()
     {
         for (int i = 0; i < gameMechanicsEntries.Count; i++)
         {
-            InitItem(gameMechanicsEntries[i], i);
+            InitItem(gameMechanicsEntries[i], gmContent.transform,i, "gm");
+        }
+        for (int i = 0; i < quantumMechanicsEntries.Count; i++)
+        {
+            InitItem(gameMechanicsEntries[i], qmContent.transform, i, "qm");
         }
         SetGM();
 
     }
 
-    void InitItem(TutorialEntry entry, int index)
+    void InitItem(TutorialEntry entry, Transform parent, int index, string tag)
     {
-        GameObject go = Instantiate(button, buttonPanel.transform);
+        GameObject go = Instantiate(button, parent);
         CustomButton cb = go.GetComponent<CustomButton>();
         cb.text.text = entry.title;
         EntrySelection es = go.GetComponent<EntrySelection>();
         es.SetSelectionManager(this);
         es.SetId(index);
+        es.tag = tag;
        
     }
 
     public void SetGM()
     {
         QM.SetAsSelection(false);
+        qmScrollArea.SetActive(false);
         S.SetAsSelection(false);
         GM.SetAsSelection(true);
+        storyArea.SetActive(false);
+        gmScrollArea.SetActive(true);
+        entryArea.SetActive(true);
     }
     public void SetQM()
     {
         GM.SetAsSelection(false);
+        gmScrollArea.SetActive(false);
         S.SetAsSelection(false);
+        storyArea.SetActive(false);
+        qmScrollArea.SetActive(true);
         QM.SetAsSelection(true);
+        entryArea.SetActive(true);
+
     }
 
     public void SetS()
     {
+        qmScrollArea.SetActive(false);
+        gmScrollArea.SetActive(false);
+        entryArea.SetActive(false);
+        storyArea.SetActive(true);
         QM.SetAsSelection(false);
         GM.SetAsSelection(false);
         S.SetAsSelection(true);
@@ -78,7 +104,34 @@ public class TutorialSelectionManager : MonoBehaviour
             selection.Deselect();
         }
         selection = entrySelection;
-        displayImage.sprite = gameMechanicsEntries[selection.id].image1;
-        infoText.text = gameMechanicsEntries[selection.id].info1;
+        if(entrySelection.tag == "gm")
+        {
+            if (gameMechanicsEntries[selection.id].image1 != null)
+            {
+                displayImage.sprite = gameMechanicsEntries[selection.id].image1;
+            }
+            else displayImage.sprite = defaultImage;
+            if (gameMechanicsEntries[selection.id].image2 != null)
+            {
+                displayImage.sprite = gameMechanicsEntries[selection.id].image1;
+            }
+            else displayImage.sprite = defaultImage;
+            infoText.text = gameMechanicsEntries[selection.id].info1;
+        }
+        if (entrySelection.tag == "qm")
+        {
+            if (quantumMechanicsEntries[selection.id].image1 != null)
+            {
+                displayImage.sprite = quantumMechanicsEntries[selection.id].image1;
+            }
+            else displayImage.sprite = defaultImage;
+            if (quantumMechanicsEntries[selection.id].image2 != null)
+            {
+                displayImage.sprite = quantumMechanicsEntries[selection.id].image1;
+            }
+            else displayImage.sprite = defaultImage;
+            infoText.text = gameMechanicsEntries[selection.id].info1;
+        }
+
     }
 }
